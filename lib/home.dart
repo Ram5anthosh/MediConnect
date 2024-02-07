ef0add8f142dbf915a9c919fdf154e7807adea52
Ram5anthosh <ramsanthosh2112@gmail.com>
2024-02-08 00:39:23 +0530

    Added home page

---
+++ lib/home.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/startup.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 35, top: 30),
+              child: Text(
+                'Indian Institute of Information Technology',
+                style: TextStyle(color: Colors.black, fontSize: 33),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+            ),
+            SingleChildScrollView(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'login');
+                    },
+                    style: ButtonStyle(),
+                    child: Text(
+                      'Sign In',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.black,
+                          fontSize: 18),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'register');
+                    },
+                    style: ButtonStyle(),
+                    child: Text(
+                      'Sign Up',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.black,
+                          fontSize: 18),
+                    ),
+                  ),
+                ],
+              )
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
